49350052455e273f264067b9288d5acfa48903c6
Tabitha Vogelaar <Tabitha Vogelaar>
2017-05-15 14:15:31 +0200

    Made an empty Window

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,17 @@
+import javax.swing.*;
+import java.awt.*;
+
 /**
  * Created by Arthur on 15-5-2017.
  */
-public class Main {
+public class Main extends JPanel {
     public static void main(String[] arghs){
-        
+        JFrame frame = new JFrame("Memory");
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.setMinimumSize(new Dimension(800,600));
+        frame.setExtendedState(frame.getExtendedState() | JFrame.MAXIMIZED_BOTH);
+        frame.setContentPane(new Main());
+        frame.setVisible(true);
+
     }
 }
