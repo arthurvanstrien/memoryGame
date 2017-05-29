4039c4098222e5de59682b79ae5d01a44aa1afa6
Arthur van Strien <arthurvanstrien@gmail.com>
2017-05-29 15:08:56 +0200

    added a getter for the card arrayList.

--- src/CardList.java
+++ src/CardList.java
@@ -5,10 +5,18 @@ import java.util.Collections;
  * Created by Tabitha on 24-5-2017.
  */
 public class CardList {
+
     public ArrayList<Card> cards;
 
+    public ArrayList<Card> getCards() {
+
+        return cards;
+    }
+
     public void Cardlist(){
+
         for (int i = 0; i <= 24; i++){
+
             Card card = new Card();//todo maybe add parameters later
             card.setIndex(i);
             cards.add(card);
@@ -17,13 +25,16 @@ public class CardList {
 
     //shuffles all the cards
     public ArrayList shuffle(){
+
         Collections.shuffle(cards);
         return cards;
     }
 
     //puts all the cards om the board/refreshes
     public void drawBoard(){
+
         for (int i = 0;  i < 24; i++){
+
             cards.get(i).putOnBoard();
         }
     }
