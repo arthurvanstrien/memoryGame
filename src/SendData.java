06d8d4055d1c6a6ed9745d969e74b20885eed8da
ArthurvanStrien <arthurvanstrien@gmail.com>
2017-06-05 22:22:32 +0200

    added a class that handles sending data to other clients.

---
+++ src/SendData.java
@@ -0,0 +1,26 @@
+import java.io.DataOutputStream;
+import java.io.IOException;
+
+/**
+ * Created by Arthur on 5-6-2017.
+ */
+public class SendData {
+
+    private boolean myTurn;
+    private DataOutputStream outputStream;
+
+    public SendData(DataOutputStream outputStream, Boolean myTurn) {
+        this.myTurn = myTurn;
+        this.outputStream = outputStream;
+    }
+
+    public void clicked(int index) {
+
+        try {
+            outputStream.writeUTF("CLICKED");
+            outputStream.write(index);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
