ca3ba6675fc6de356fe0049c5503a2c9953504c3
ArthurvanStrien <arthurvanstrien@gmail.com>
2017-06-05 21:03:15 +0200

    The image icons are created once now instead of multiple times.

--- src/Card.java
+++ src/Card.java
@@ -1,14 +1,17 @@
 import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.IOException;
 
 /**
  * Created by Tabitha on 23-5-2017.
  */
-public class Card {
+public class Card implements ActionListener{
     private String name;
-    private String imagePath;
+    private ImageIcon image;
+    private ImageIcon backImage;
     private boolean faceDown;
     private boolean onBoard;
     private int index; //place on the board and index in CardsList
@@ -17,16 +20,25 @@ public class Card {
     public Card(String name, int index){
         this.name = name;
         this.index = index;
-        imagePath = "resources/images/" + name +".jpg";
         onBoard = true;
         faceDown = true;
 
+        //Create the imageIcon for this specific card.
         try {
-            Image back = ImageIO.read(getClass().getResource("/resources/images/back.jpg"));
-            button = new JButton(new ImageIcon(back));
+            image = new ImageIcon(ImageIO.read(getClass().getResource("/resources/images/" + name + ".jpg")));
         } catch (IOException e) {
             e.printStackTrace();
         }
+
+        //Create the imageIcon that is used as back by all cards.
+        try {
+            backImage = new ImageIcon(ImageIO.read(getClass().getResource("/resources/images/back.jpg")));
+            button = new JButton(backImage);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        button.addActionListener(this);
     }
 
     public int getIndex() {
